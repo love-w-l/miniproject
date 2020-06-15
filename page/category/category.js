def8e65d30fee4285377b7bf537bb57a8be92dc0
love-w-l <2861519527@qq.com>
2020-06-15 15:35:15 +0800

    项目最初

--- page/category/category.js
+++ page/category/category.js
@@ -1,62 +1,55 @@
-// page/home/about.js
-Page({
 
+// 获取全局实例中的数据
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    list:[]
   },
-
+  getuserinfo(event){
+    // console.log(value)
+    // console.log(event)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  // 监听页面的滚动
+  onPageScroll(options){
+    // console.log(options)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
+   /**
+   * 页面上拉触底事件的处理函数
    */
-  onReady: function () {
-
+  onReachBottom: function () {
+    console.log('滚动到底部了')
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('下拉刷新')
   },
-
   /**
-   * 页面上拉触底事件的处理函数
+   * 生命周期函数--监听页面初次渲染完成
    */
-  onReachBottom: function () {
-
+  onReady: function () {
   },
-
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  },
   /**
    * 用户点击右上角分享
    */
