d06402fbc0428b0b96d3e908775a15eeeee91330
love-w-l <2861519527@qq.com>
2020-06-15 15:17:34 +0800

    app注册小程序实例和page解读

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+// 注册小程序实例
+// 一般不在app.js中进行数据请求,一般是在对应的页面进行数据请求
+// 在这个页面一般进行设置小程序进入场景对应的设置,还有设置全局数据(在每个页面中都能访问的数据),
 App({
-  onLaunch:()=>{
+  onLaunch:(options)=>{
+// 在这里也可以获取对应的进入方法
+
     //可在此函数内进行网络请求数据
     // console.log('小程序初始化时执行')
 
-    //获取用户的信息
+    //获取用户的信息 可能在以后的版本中废除,要想获取用户的信息,必须使用按钮当用户点击时获取,在分类页面进行演示
     // wx.getUserInfo({
     //   complete: (res) => {
     //     console.log(res)
@@ -16,7 +21,16 @@ App({
     //   throw err
     // }, 3000);
   },
-  onShow:()=>{
+  onShow:(options)=>{
+    // options获取用户在哪里进的小程序做一个对应的处理
+    // console.log(options)
+    // switch(options.scene){
+    //   case 1001:
+    //     break
+    //   case 1002: 
+    //     break
+    // }
+  
     // 在页面显示出来时调用的函数
     // console.log('页面显示出来了')
   },
@@ -27,5 +41,9 @@ App({
   onError(){
     // 数据请求失败时调用
     // console.log('数据请求失败')
+  },
+  globalData:{
+    name:'codewhy',
+    age:30
   }
 })
--- page/category/category.js
+++ page/category/category.js
@@ -1,62 +1,71 @@
-// page/category/category.js
-Page({
 
+// 获取全局实例中的数据
+const app = getApp()
+console.log(app.globalData.name)
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-
+    message:app.globalData.name,
+    list:[]
   },
-
+  getuserinfo(event){
+    // console.log(value)
+    // console.log(event)
+  },
+  click(){
+    console.log('codewhy')
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.request({
+      url: 'http://152.136.185.210:8000/api/n3/home/data?type=pop&page=1',
+      success:(res)=>{
+        this.setData({
+          list:res.data.data.list
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
+  // 监听页面的滚动
+  onPageScroll(options){
+    // console.log(options)
+  },
+   /**
+   * 页面上拉触底事件的处理函数
    */
-  onReady: function () {
-
+  onReachBottom: function () {
+    console.log('滚动到底部了')
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('下拉刷新')
   },
-
   /**
-   * 页面上拉触底事件的处理函数
+   * 生命周期函数--监听页面初次渲染完成
    */
-  onReachBottom: function () {
-
+  onReady: function () {
   },
-
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  },
   /**
    * 用户点击右上角分享
    */
